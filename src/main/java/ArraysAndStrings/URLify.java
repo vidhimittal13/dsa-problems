c9e744be5f1e9ce3c9c71d62afce760672d7fc87
Vidhi Mittal <vidhi.mittal@emory.edu>
2023-06-06 17:40:57 +0530

    URLify.java

--- src/main/java/ArraysAndStrings/URLify.java
+++ src/main/java/ArraysAndStrings/URLify.java
@@ -2,6 +2,10 @@ package ArraysAndStrings;
 public class URLify {
     public static void makeURL(char[] str, int trueLength)
     {
+        //Write a method to replace all spaces in a string with '%20'. You may assume that the string
+        //has sufficient space at the end to hold the additional characters, and that you are given the "true"
+        //length of the string. (Note: If implementing in Java, please use a character array so that you can
+        //perform this operation in place.)
         int index = str.length;
         for (int i = trueLength-1; i >=0 ; i--) {
             if(str[i]==' ')
