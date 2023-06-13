2d7cbec388961e5872f77a353e93a267ef9e4ba2
Vidhi Mittal <vidhi.mittal@emory.edu>
2023-06-13 15:50:27 +0530

    ZeroMatrix.java

--- src/main/java/ArraysAndStrings/ZeroMatrix.java
+++ src/main/java/ArraysAndStrings/ZeroMatrix.java
@@ -11,7 +11,7 @@ public class ZeroMatrix {
         boolean[] row = new boolean[matrix.length];
         boolean[] col = new boolean[matrix[0].length];
 
-        //Storing the rows and columns with 0 values
+        //Storing the rows and columns with 0 values.
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 if(matrix[i][j]==0)
